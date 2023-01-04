e5aed36f93d24ceee62052858e1d04c654a03cb1
524991368@qq.com <524991368@qq.com>
2023-01-04 13:42:32 +0800

    根据学习进度更新笔记

--- src/main/scala/com/pk/scala/collection/MapApp.scala
+++ src/main/scala/com/pk/scala/collection/MapApp.scala
@@ -1,29 +1,31 @@
 package com.pk.scala.collection
 
 /**
-  * Scala中Map：和Java类似  KV 映射
-  * 不可变
-  * 可变
-  */
+ * Scala中Map：和Java类似  KV 映射
+ * <p>
+ * 不可变
+ * <p>
+ * 可变
+ */
 object MapApp {
-
   /**
-    * abstract class Option
-    * case class Some extends Option   取到
-    * case object None extends Option[Nothing]  没取到
-    */
+   * abstract class Option
+   * <p>
+   * case class Some extends Option   取到
+   * <p>
+   * case object None extends Option[Nothing]  没取到
+   */
   def main(args: Array[String]): Unit = {
-    val map = Map("pk"->31, "xiaohong"->18)
+    val map = Map("pk" -> 31, "xiaohong" -> 18)
+    for ((k,v) <- map) {
+      println(k+"-------->"+v)
+    }
     val value = map.getOrElse("pk1", -99) // 在map取值中要注意的，建议使用getOrElse
     println(value)
-
-
-    val b = scala.collection.mutable.Map("pk"->31, "xiaohong"->18)
-    val c = scala.collection.mutable.Map[String,Int]()
+    val b = scala.collection.mutable.Map("pk" -> 31, "xiaohong" -> 18)
+    val c = scala.collection.mutable.Map[String, Int]()
     b += ("zhangsan" -> 10)
     b.put("lisi", 20)
-
     b -= "lisi"
   }
-
 }
