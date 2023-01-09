dc6f0f291dbff4c67abf05ff33516055ac0a3bfa
524991368@qq.com <524991368@qq.com>
2023-01-09 13:38:57 +0800

    根据学习进度更新笔记

--- src/main/java/com/pk/mr/wc/WordCountMapper.java
+++ src/main/java/com/pk/mr/wc/WordCountMapper.java
@@ -1,6 +1,5 @@
 package com.pk.mr.wc;
 
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -9,19 +8,20 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 /**
- *
  * KEYIN：输入数据的KEY的数据类型
+ * <p>
  * VALUEIN：输入数据的VALUE的数据类型
+ * <p>
  * KEYOUT：输出数据的KEY的数据类型
+ * <p>
  * VALUEOUT：输出数据的VALUE的数据类型
- *
+ * <p>
  * LongWritable：就是文件中数据的偏移量
+ * <p>
  * Text：每一行数据的内容
  */
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-
     IntWritable ONE = new IntWritable(1);
-
 
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
@@ -34,23 +34,20 @@ public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritabl
     }
 
     /**
-     * @param key 每行数据的偏移量，不是行号
-     * @param value 每行数据
+     * @param key     每行数据的偏移量，不是行号
+     * @param value   每行数据
      * @param context
      * @throws IOException
      * @throws InterruptedException
      */
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
-//        System.out.println("key ====>" + key.get());
-//        System.out.println("value ====>" + value.toString());
-
+        System.out.println("key ====>" + key.get());
+        System.out.println("value ====>" + value.toString());
         // 获取每行数据，然后按照指定分隔符进行切分
         String[] splits = value.toString().split(",");
-
         // 循环输出
-        for(String word : splits) {
+        for (String word : splits) {
             context.write(new Text(word), ONE);
         }
     }
--- src/main/java/com/pk/mr/wc/WordCountReducer.java
+++ src/main/java/com/pk/mr/wc/WordCountReducer.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         System.out.println("hello");
@@ -15,17 +14,20 @@ public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritab
 
     /**
      * Iterable<IntWritable> values
-     *
+     * <p>
      * pk,pk,pk    hello,hello   world
-     * <pk,1> <pk,1> <pk,1>  ==> <pk, <1,1,1>>
-     * <hello,1> <hello,1>   ==> <hello, <1,1>>
-     * <world,1>             ==> <world, <1>>
-     *
+     * <p>
+     * <pk,1> <pk,1> <pk,1> ==> <pk,<1,1,1>>
+     * <p>
+     * <hello,1> <hello,1> ==> <hello, <1,1>>
+     * <p>
+     * <world,1>==> <world, <1>>
+     * <p>
      * 相同的key经过shuffle之后，会在同一个reduce中进行聚合
-     *
+     * <p>
      * 默认字典序
      *
-     * @param word  单词
+     * @param word    单词
      * @param values
      * @param context
      * @throws IOException
@@ -34,12 +36,10 @@ public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritab
     @Override
     protected void reduce(Text word, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int count = 0;
-
         // 累加求和
-        for(IntWritable value : values) {
+        for (IntWritable value : values) {
             count += value.get();
         }
-
         // 输出
         context.write(word, new IntWritable(count));
     }
